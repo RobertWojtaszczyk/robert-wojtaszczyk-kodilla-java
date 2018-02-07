7d292a5e14f087f4d2d25882669e7537bbb98000
RobertWojtaszczyk <robert.wojtaszczyk@gmail.com>
2018-02-07 15:15:50 +0100

    Testing #3 6.3 OddNumbersExterminator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,26 +1,16 @@
 package com.kodilla.testing;
-import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.collection.OddNumbersExterminator;
 import com.kodilla.testing.calculator.Calculator;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-        String result = simpleUser.getUsername();
-        System.out.print("Testing class SimpleUser: ");
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
         Calculator calculator = new Calculator();
         // testing methods separately
         System.out.print("Testing class Calculator, add() method: ");
         System.out.println((calculator.add(49, 51) == 100) ? "test OK" : "Error!");
         System.out.print("Testing class Calculator, subtract() method: ");
         System.out.println((calculator.subtract(51, 49) == 2) ? "test OK" : "Error!");
-        // or both methods simultaneously
-        System.out.print("Testing class Calculator: ");
-        System.out.println(calculator.add(49, 51) == 100 && calculator.subtract(51, 49) == 2 ? "test OK" : "Error!");
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.testing.collection;
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Test Case: begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test Suite: begin");
+    }
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test Suite: end");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+        List<Integer> integerNumbersList = new ArrayList<>();
+        List<Integer> evenNumbersList = new ArrayList<>();
+        OddNumbersExterminator testEmptyList = new OddNumbersExterminator();
+        evenNumbersList = testEmptyList.exterminate(integerNumbersList);
+        System.out.println("Testing empty list");
+        Assert.assertTrue(evenNumbersList.isEmpty());
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+        List<Integer> integerNumbersList = new ArrayList<>();
+        List<Integer> evenNumbersList = new ArrayList<>();
+        for (int i = 0; i < 11; i++){
+            integerNumbersList.add(i);
+        }
+        OddNumbersExterminator testFilledList = new OddNumbersExterminator();
+        evenNumbersList = testFilledList.exterminate(integerNumbersList);
+        System.out.println("Testing filled list");
+        for (Integer number : evenNumbersList){
+            Assert.assertEquals(0, number % 2);
+        }
+    }
+}
