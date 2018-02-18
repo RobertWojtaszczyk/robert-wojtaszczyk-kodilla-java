2aa07eb88f5ddd96dbd76abcdc1c9c3a4f385a15
RobertWojtaszczyk <robert.wojtaszczyk@gmail.com>
2018-02-18 09:16:53 +0100

    7.6 Portfolio v1

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,14 +1,13 @@
 package com.kodilla.stream.array;
-import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
         IntStream.range(0,numbers.length)
                 .forEach(n -> System.out.println(numbers[n]));
-        OptionalDouble average = IntStream.range(0,numbers.length)
+        double average = IntStream.range(0,numbers.length)
                 .map(n -> numbers[n])
-                .average();
-        return average.getAsDouble();
+                .average().getAsDouble();
+        return average;
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -3,7 +3,7 @@ import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
-public class World {
+public final class World {
     private final Set<Continent> continents = new HashSet<>();
     public void addContinent(Continent continent) {
         continents.add(continent);
