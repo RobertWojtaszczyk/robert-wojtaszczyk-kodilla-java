d7add905cdb40a526bb1a9da640b40747bb8ede6
RobertWojtaszczyk <robert.wojtaszczyk@gmail.com>
2018-04-10 11:01:49 +0200

    iterable & exceptions

--- portfolio/src/test/java/com/kodilla/linkedlist/LinkedListTestSuite.java
+++ portfolio/src/test/java/com/kodilla/linkedlist/LinkedListTestSuite.java
@@ -6,52 +6,40 @@ public class LinkedListTestSuite {
     @Test
     public void testLinkedList() {
         Collection<String> collection = new Collection<>();
+
         collection.add("first");
         collection.add("second");
-        System.out.println(collection.get(1));
-        System.out.println(collection.get(0));
-        System.out.println(collection.size());
-        System.out.println(collection.remove(0));
-        System.out.println(collection.get(0));
-        System.out.println(collection.size());
+        collection.add("third");
+        collection.add("fourth");
+        System.out.println("Size: " + collection.size() + ":" + collection.getSize());
+
+        for (String element : collection) {
+            System.out.println(element);
+        }
+
+        System.out.println("Element(0): " + collection.get(0));
+        System.out.println("Element(1): " + collection.get(1));
+        System.out.println("Removing element(0): " + collection.remove(0));
+        System.out.println("Element(0): " + collection.get(0));
+        System.out.println("Size: " + collection.size() + ":" + collection.getSize());
+        System.out.println("Removing element(2): " + collection.remove(1));
+        System.out.println("Size: " + collection.size() + ":" + collection.getSize());
+        System.out.println("Removing element(2): " + collection.remove(1));
+        System.out.println("Size: " + collection.size() + ":" + collection.getSize());
+
+        //System.out.println("Element(1): " + collection.get(1));
 
 
+        System.out.println("Removing element(0): " + collection.remove(0));
+        System.out.println("Size: " + collection.size() + ":" + collection.getSize());
 
-
-
-
-        /*int numberOfElements = 10;
-        //wypełnienie kolekcji
-        for (int i = 0; i < numberOfElements; i++) {
-            //collection.add(new Element<String>("Test" + i));
-            //collection.add(new Element<>(i));
-            collection.add(i);
+        collection.add("first");
+        collection.add("second");
+        collection.add("third");
+        collection.add("fourth");
+        for (String element : collection) {
+            System.out.println(element);
         }
-        // wyświetlenie wszystkich elementów kolekcji, w pętli celowo przekraczam zakres
-        for (int i = 0; i < collection.size() + 1; i++) {
-            System.out.println(collection.get(i));
-        }
-        System.out.println("Collection size: " + collection.size());
-        //test na usuwanie n - tego elementu
-        if (collection.remove(8)) {
-            System.out.println("\nElement removed successfully. Current collection size is " + collection.size() + ":");
-        }
-        for (int i = 0; i < collection.size(); i++) {
-            System.out.println(collection.get(i));
-        }
-        //test na usuwanie ostatniego elementu
-        if (collection.remove(collection.size()-1)) {
-            System.out.println("\nLast element removed successfully. Current collection size is " + collection.size() + ":");
-        }
-        for (int i = 0; i < collection.size(); i++) {
-            System.out.println(collection.get(i));
-        }
-        //test na usuwanie pierwszego elementu
-        if (collection.remove(0)) {
-            System.out.println("\nFirst element removed successfully. Current collection size is " + collection.size() + ":");
-        }
-        for (int i = 0; i < collection.size(); i++) {
-            System.out.println(collection.get(i));
-        }*/
+        System.out.println("Size: " + collection.size() + ":" + collection.getSize());
     }
 }
